b378adfdb956a2c812649ac038978b5b49b09ed9
MinuraWije <minurawije@gmail.com>
2024-03-21 18:16:24 +0530

    Completed project.

---
+++ src/main/java/lk/ijse/bookworm/Dto/Tm/BorrowTm.java
@@ -0,0 +1,19 @@
+package lk.ijse.bookworm.Dto.Tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class BorrowTm {
+    private String memberId;
+    private String bookId;
+    private String status;
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+}
